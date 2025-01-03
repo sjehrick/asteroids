fcc15928ad2357aebfe0a4eb7d3f566f72ddd3a8
sjehrick <samuel.ehrick@gmail.com>
2025-01-02 19:59:46 -0500

    limit fps to 60 via clock function

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ from constants import *
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        
+        
+        dt = clock.tick(60) / 1000 
+
 
     
 
