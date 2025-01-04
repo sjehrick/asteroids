de7cd25b3e25a98f1295d864fa1d9270b4490ad5
sjehrick <samuel.ehrick@gmail.com>
2025-01-04 16:59:54 -0500

    added split logic to asteroid.py, main

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,16 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        new_vector1 = self.velocity.rotate(random_angle)
+        new_vector2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid2= Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid1.velocity = new_vector1 * 1.2
+        new_asteroid2.velocity = new_vector2 * 1.2
--- main.py
+++ main.py
@@ -47,12 +47,12 @@ def main():
             for shot in shots:
                 if asteroid.get_collisions(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         pygame.display.flip()
         
         
-        dt = clock.tick(60) / 1000 
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
